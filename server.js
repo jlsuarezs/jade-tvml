8c45e5d38589043f19dc14fd4c7c332f07c50704
Andrew Carr <andrew.carr@betterbills.com>
2015-10-22 19:35:44 +1100

    Andrew/James: Refactor into functions

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express'),
     logger = require('morgan'),
     jade = require('jade');
 
+var templateFilePath = require('./lib/templateFilePath'),
+    tvJSTemplateWrapper = require('./lib/tvJSTemplateWrapper');
+
 var app = express();
 
 app.use(express.static('public'));
@@ -12,11 +15,19 @@ app.set('views', __dirname + "/templates");
 
 app.get('/template/:path', function (req, res) {
   var baseUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  var template = jade.renderFile(templateFilePath(req.params.path), {
+    doctype: 'xml',
+    baseUrl: baseUrl,
+    things: [
+      'One Thing',
+      'Two Thing',
+      'Red Thing',
+      'Blue Thing'
+    ]
+  });
 
   res.header('Content-Type', 'application/javascript');
-  var template = jade.renderFile('./templates/' + req.params.path + '.jade', { doctype: 'xml', baseUrl: baseUrl, things: ['One Thing', 'Two Thing', 'Red Thing', 'Blue Thing'] });
-
-  res.send("var Template = function () { return '" + template + "'}")
+  res.send(tvJSTemplateWrapper(template));
 });
 
 var server = app.listen(5000);
